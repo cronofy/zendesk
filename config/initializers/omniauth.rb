940bfb627b063e90db4aa47ead3162f29f89ed67
Adam Bird <adam.bird@gmail.com>
2016-05-27 09:24:10 +0100

    limit to one calendar account

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :cronofy, ENV["CRONOFY_CLIENT_ID"], ENV["CRONOFY_CLIENT_SECRET"], {
-    scope: "read_account list_calendars read_events create_event delete_event"
+    scope: "read_account list_calendars read_events create_event delete_event",
+    authorize_params: { avoid_linking: 1 },
   }
 
   provider :zendesk, ENV['ZENDESK_CLIENT_ID'], ENV['ZENDESK_CLIENT_SECRET'], {
